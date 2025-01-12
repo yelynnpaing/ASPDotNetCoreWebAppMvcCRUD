ed7b2e47ae146639f6d60d3d17d7720c3769c78f
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2025-01-12 20:08:30 +0630

    Add - Read data

---
+++ ASPDotNetCoreWebAppMvcCRUD/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using ASPDotNetCoreWebAppMvcCRUD.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPDotNetCoreWebAppMvcCRUD.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public ProductController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = context.Products.OrderByDescending(p => p.Id).ToList();
+            return View(products);
+        }
+    }
+}
