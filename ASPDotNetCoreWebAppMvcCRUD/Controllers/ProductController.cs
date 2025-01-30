94c4e0daf37e3a55ab66810e29ce5f24953548e7
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2025-01-30 22:46:03 +0630

    Add - Create product

--- ASPDotNetCoreWebAppMvcCRUD/Controllers/ProductController.cs
+++ ASPDotNetCoreWebAppMvcCRUD/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using ASPDotNetCoreWebAppMvcCRUD.Services;
+﻿using ASPDotNetCoreWebAppMvcCRUD.Models;
+using ASPDotNetCoreWebAppMvcCRUD.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASPDotNetCoreWebAppMvcCRUD.Controllers
@@ -6,14 +7,60 @@ namespace ASPDotNetCoreWebAppMvcCRUD.Controllers
     public class ProductController : Controller
     {
         private readonly ApplicationDbContext context;
-        public ProductController(ApplicationDbContext context)
+        private readonly IWebHostEnvironment environment;
+        public ProductController(ApplicationDbContext context, IWebHostEnvironment environment)
         {
             this.context = context;
+            this.environment = environment;
         }
         public IActionResult Index()
         {
             var products = context.Products.OrderByDescending(p => p.Id).ToList();
             return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductDto productDto)
+        {
+            if(productDto.ImageFile == null)
+            {
+                ModelState.AddModelError("ImageFile", "Image file is require.");
+            }
+            if(!ModelState.IsValid)
+            {
+                return View(productDto);
+            }
+
+            //Save image file
+            string newFileName = DateTime.Now.ToString("yyMMddHHmmssfff");
+            newFileName += Path.GetExtension(productDto.ImageFile!.FileName);
+            string imageFullPath = environment.WebRootPath + "/Products/" + newFileName;
+            using (var stream = System.IO.File.Create(imageFullPath))
+            {
+                productDto.ImageFile.CopyTo(stream);
+            }
+
+            //save new product into database
+            Product product = new Product()
+            {
+                Name = productDto.Name,
+                Brand = productDto.Brand,
+                Category = productDto.Category,
+                Price = productDto.Price,
+                Description = productDto.Description,
+                ImageFileName = newFileName,
+                CreatedAt = DateTime.Now
+            };
+
+            context.Products.Add(product);
+            context.SaveChanges();
+
+            return RedirectToAction("Index", "Product");
         }
     }
 }
---
+++ ASPDotNetCoreWebAppMvcCRUD/Models/ProductDto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASPDotNetCoreWebAppMvcCRUD.Models
+{
+    public class ProductDto
+    {
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = "";
+        [Required, MaxLength(100)]
+        public string Brand { get; set; } = "";
+        [Required, MaxLength(100)]
+        public string Category { get; set; } = "";
+        [Required]
+        public decimal Price { get; set; }
+        public string Description { get; set; } = "";
+        public IFormFile? ImageFile { get; set; }
+    }
+}
