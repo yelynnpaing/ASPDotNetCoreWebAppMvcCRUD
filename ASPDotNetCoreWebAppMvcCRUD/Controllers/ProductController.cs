f31040aea95546f7e502c7dfea455a86ef759c92
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2025-01-31 23:44:49 +0630

    Add - Edit and Delete product

--- ASPDotNetCoreWebAppMvcCRUD/Controllers/ProductController.cs
+++ ASPDotNetCoreWebAppMvcCRUD/Controllers/ProductController.cs
@@ -62,5 +62,92 @@ namespace ASPDotNetCoreWebAppMvcCRUD.Controllers
 
             return RedirectToAction("Index", "Product");
         }
+
+        public IActionResult Edit(int id)
+        {
+            var product = context.Products.Find(id);
+
+            if(product is null)
+            {
+                return RedirectToAction("Index", "Product");
+            }
+
+            //create productDto to product
+            ProductDto productDto = new ProductDto()
+            {
+                Name = product.Name,
+                Brand = product.Brand,
+                Category = product.Category,
+                Price = product.Price,
+                Description = product.Description
+            };
+
+            ViewData["ProductId"] = product.Id;
+            ViewData["ImageFileName"] = product.ImageFileName;
+            ViewData["CreatedAt"] = product.CreatedAt.ToString("MM/dd/yy");
+
+            return View(productDto);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, ProductDto productDto)
+        {
+            var product = context.Products.Find(id);
+            if(product is null)
+            {
+                return RedirectToAction("Index", "Product");
+            }
+
+            if(!ModelState.IsValid)
+            {
+                ViewData["Productid"] = product.Id;
+                ViewData["ImageFileName"] = product.ImageFileName;
+                ViewData["CreatedAt"] = product.CreatedAt.ToString("MM/dd/yy");
+
+                return View(productDto);
+            }
+
+            //update image file if new image file had arrive
+            string newFileName = product.ImageFileName;
+            if(productDto.ImageFile != null)
+            {
+                newFileName = DateTime.Now.ToString("yyMMddHHmmssfff");
+                newFileName += Path.GetExtension(productDto.ImageFile.FileName);
+                string imageFullPath = environment.WebRootPath + "/Products/" + newFileName; 
+                using (var stream = System.IO.File.Create(imageFullPath))
+                {
+                    productDto.ImageFile.CopyTo(stream);
+                }
+
+                //delete old image
+                string oldImageFullPath = environment.WebRootPath + "/Products/" + product.ImageFileName;
+                System.IO.File.Delete(oldImageFullPath);
+            }
+
+            //update product data
+            product.Name = productDto.Name;
+            product.Brand = productDto.Brand;
+            product.Category = productDto.Category;
+            product.Price = productDto.Price;
+            product.Description = productDto.Description;
+            product.ImageFileName = newFileName;
+
+            context.SaveChanges();
+            return RedirectToAction("Index", "Product");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = context.Products.Find(id);
+            if(product is null)
+            {
+                return RedirectToAction("Index", "Product");
+            }
+            string imageFullPath = environment.WebRootPath + "/Products/" + product.ImageFileName;
+            System.IO.File.Delete(imageFullPath);
+            context.Products.Remove(product);
+            context.SaveChanges(true);
+            return RedirectToAction("Index", "Product");
+        }
     }
 }
